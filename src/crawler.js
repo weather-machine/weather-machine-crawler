c719ab9a0f61fd9f8a1aec9576ed3146a2963746
Marcin Brandys <marcin.brandys.ext@gigaset.com>
2018-11-02 16:14:28 +0100

    Added weather class

--- src/crawler.js
+++ src/crawler.js
@@ -8,47 +8,180 @@ var config = {
     intervalDuration: 2000,
     pages: [
         {
-            city: 'London',
             url: 'http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=212e48f40836a854c1a266834563a0b5'
         },
         {
-            city: 'Warsaw',
             url: 'http://api.openweathermap.org/data/2.5/weather?q=Warsaw,pl&appid=212e48f40836a854c1a266834563a0b5'
         },
         {
-            city: 'Wrocław',
             url: 'http://api.openweathermap.org/data/2.5/weather?q=Wroclaw,pl&appid=212e48f40836a854c1a266834563a0b5'
         }
     ]
 };
+var Weather = /** @class */ (function () {
+    function Weather(date, placeId, weatherTypeId, windDirectionId, temperature, temperatureMax, temperatureMin, skyDescription, humidityPercent, pressureMb, windSpeed, isForecast) {
+        this._date = date;
+        this._placeId = placeId;
+        this._weatherTypeId = weatherTypeId;
+        this._windDirectionId = windDirectionId;
+        this._temperature = temperature;
+        this._temperatureMax = temperatureMax;
+        this._temperatureMin = temperatureMin;
+        this._skyDescription = skyDescription;
+        this._humidityPercent = humidityPercent;
+        this._pressureMb = pressureMb;
+        this._windSpeed = windSpeed;
+        this._isForecast = isForecast;
+    }
+    Object.defineProperty(Weather.prototype, "date", {
+        get: function () {
+            return this._date;
+        },
+        set: function (value) {
+            this._date = value;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(Weather.prototype, "placeId", {
+        get: function () {
+            return this._placeId;
+        },
+        set: function (value) {
+            this._placeId = value;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(Weather.prototype, "weatherTypeId", {
+        get: function () {
+            return this._weatherTypeId;
+        },
+        set: function (value) {
+            this._weatherTypeId = value;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(Weather.prototype, "windDirectionId", {
+        get: function () {
+            return this._windDirectionId;
+        },
+        set: function (value) {
+            this._windDirectionId = value;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(Weather.prototype, "temperature", {
+        get: function () {
+            return this._temperature;
+        },
+        set: function (value) {
+            this._temperature = value;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(Weather.prototype, "temperatureMax", {
+        get: function () {
+            return this._temperatureMax;
+        },
+        set: function (value) {
+            this._temperatureMax = value;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(Weather.prototype, "temperatureMin", {
+        get: function () {
+            return this._temperatureMin;
+        },
+        set: function (value) {
+            this._temperatureMin = value;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(Weather.prototype, "skyDescription", {
+        get: function () {
+            return this._skyDescription;
+        },
+        set: function (value) {
+            this._skyDescription = value;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(Weather.prototype, "humidityPercent", {
+        get: function () {
+            return this._humidityPercent;
+        },
+        set: function (value) {
+            this._humidityPercent = value;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(Weather.prototype, "pressureMb", {
+        get: function () {
+            return this._pressureMb;
+        },
+        set: function (value) {
+            this._pressureMb = value;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(Weather.prototype, "windSpeed", {
+        get: function () {
+            return this._windSpeed;
+        },
+        set: function (value) {
+            this._windSpeed = value;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(Weather.prototype, "isForecast", {
+        get: function () {
+            return this._isForecast;
+        },
+        set: function (value) {
+            this._isForecast = value;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    return Weather;
+}());
 http.createServer().listen(config.port, config.ip);
 console.log('Server running at http://' + config.ip + ':' + config.port + '/');
-function gatherData() {
-    var _loop_1 = function (page) {
-        http.get(page.url, function (res) {
-            var data = '';
-            res.on('data', function (chunk) {
-                data += chunk;
-            });
-            res.on('end', function () {
-                prepareData(data, page);
-            });
-        }).on('error', function (error) {
-            console.log('error with: ' + page + '\n' + error.message);
-        });
-    };
-    for (var _i = 0, _a = config.pages; _i < _a.length; _i++) {
-        var page = _a[_i];
-        _loop_1(page);
-    }
-}
-setInterval(gatherData, config.intervalDuration);
-function prepareData(data, page) {
-    var parsedData = JSON.parse(data);
-    if (_.has(parsedData, 'main.temp')) {
-        console.log(_.now() + ' ' + page.city + ': ' + convertToCelsius(parsedData.main.temp) + ' Celsius');
-    }
-}
-function convertToCelsius(temperature) {
-    return (temperature - 273.15).toFixed(2);
-}
+var weather = new Weather(1541168536046, 10, 103, 34, 35.5, 32.0, 36.6, 'It will be funny', 37.1, 1003.5, 5.5, 0);
+console.log(weather);
+// function gatherData() {
+//     for (let page of config.pages) {
+//         http.get(page.url, function(res) {
+//             let data: string = '';
+//             res.on('data', function(chunk) {
+//                 data += chunk;
+//             });
+//             res.on('end', function() {
+//                 prepareData(data, page);
+//             });
+//         }).on('error', function(error) {
+//             console.log('error with: ' + page + '\n' + error.message);
+//         });
+//     }
+// } setInterval(gatherData, config.intervalDuration);
+//
+// function prepareData(data: string, page: any) {
+//     let parsedData: any = JSON.parse(data);
+//     if (_.has(parsedData, 'main.temp')) {
+//         console.log(_.now() + ' ' + page.city + ': ' + convertToCelsius(parsedData.main.temp) + ' Celsius');
+//     }
+// }
+//
+// function convertToCelsius(temperature: number) {
+//     return (temperature - 273.15).toFixed(2);
+// }
--- src/crawler.ts
+++ src/crawler.ts
@@ -9,47 +9,185 @@ const config = {
     intervalDuration: 2000,
     pages: [
         {
-            city: 'London',
             url: 'http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=212e48f40836a854c1a266834563a0b5'
         },
         {
-            city: 'Warsaw',
             url: 'http://api.openweathermap.org/data/2.5/weather?q=Warsaw,pl&appid=212e48f40836a854c1a266834563a0b5'
         },
         {
-            city: 'Wrocław',
             url: 'http://api.openweathermap.org/data/2.5/weather?q=Wroclaw,pl&appid=212e48f40836a854c1a266834563a0b5'
         }
     ]
 };
 
+class Weather {
+    private _date: number;
+    private _placeId: number;
+    private _weatherTypeId: number;
+    private _windDirectionId: number;
+    private _temperature: number;
+    private _temperatureMax: number;
+    private _temperatureMin: number;
+    private _skyDescription: string;
+    private _humidityPercent: number;
+    private _pressureMb: number;
+    private _windSpeed: number;
+    private _isForecast: number;
+
+    constructor(date: number, placeId: number, weatherTypeId: number, windDirectionId: number, temperature: number, temperatureMax: number, temperatureMin: number, skyDescription: string, humidityPercent: number, pressureMb: number, windSpeed: number, isForecast: number) {
+        this._date = date;
+        this._placeId = placeId;
+        this._weatherTypeId = weatherTypeId;
+        this._windDirectionId = windDirectionId;
+        this._temperature = temperature;
+        this._temperatureMax = temperatureMax;
+        this._temperatureMin = temperatureMin;
+        this._skyDescription = skyDescription;
+        this._humidityPercent = humidityPercent;
+        this._pressureMb = pressureMb;
+        this._windSpeed = windSpeed;
+        this._isForecast = isForecast;
+    }
+
+    get date(): number {
+        return this._date;
+    }
+
+    set date(value: number) {
+        this._date = value;
+    }
+
+    get placeId(): number {
+        return this._placeId;
+    }
+
+    set placeId(value: number) {
+        this._placeId = value;
+    }
+
+    get weatherTypeId(): number {
+        return this._weatherTypeId;
+    }
+
+    set weatherTypeId(value: number) {
+        this._weatherTypeId = value;
+    }
+
+    get windDirectionId(): number {
+        return this._windDirectionId;
+    }
+
+    set windDirectionId(value: number) {
+        this._windDirectionId = value;
+    }
+
+    get temperature(): number {
+        return this._temperature;
+    }
+
+    set temperature(value: number) {
+        this._temperature = value;
+    }
+
+    get temperatureMax(): number {
+        return this._temperatureMax;
+    }
+
+    set temperatureMax(value: number) {
+        this._temperatureMax = value;
+    }
+
+    get temperatureMin(): number {
+        return this._temperatureMin;
+    }
+
+    set temperatureMin(value: number) {
+        this._temperatureMin = value;
+    }
+
+    get skyDescription(): string {
+        return this._skyDescription;
+    }
+
+    set skyDescription(value: string) {
+        this._skyDescription = value;
+    }
+
+    get humidityPercent(): number {
+        return this._humidityPercent;
+    }
+
+    set humidityPercent(value: number) {
+        this._humidityPercent = value;
+    }
+
+    get pressureMb(): number {
+        return this._pressureMb;
+    }
+
+    set pressureMb(value: number) {
+        this._pressureMb = value;
+    }
+
+    get windSpeed(): number {
+        return this._windSpeed;
+    }
+
+    set windSpeed(value: number) {
+        this._windSpeed = value;
+    }
+
+    get isForecast(): number {
+        return this._isForecast;
+    }
+
+    set isForecast(value: number) {
+        this._isForecast = value;
+    }
+}
+
 http.createServer().listen(config.port, config.ip);
 console.log('Server running at http://' + config.ip + ':' + config.port + '/');
+let weather = new Weather(
+    1541168536046,
+    10,
+    103,
+    34,
+    35.5,
+    32.0,
+    36.6,
+    'It will be funny',
+    37.1,
+    1003.5,
+    5.5,
+    0
+);
+console.log(weather);
 
-function gatherData() {
-    for (let page of config.pages) {
-        http.get(page.url, function(res) {
-            let data: string = '';
-            res.on('data', function(chunk) {
-                data += chunk;
-            });
-            res.on('end', function() {
-                prepareData(data, page);
-            });
-        }).on('error', function(error) {
-            console.log('error with: ' + page + '\n' + error.message);
-        });
-    }
-} setInterval(gatherData, config.intervalDuration);
-
-function prepareData(data: string, page: any) {
-    let parsedData: any = JSON.parse(data);
-    if (_.has(parsedData, 'main.temp')) {
-        console.log(_.now() + ' ' + page.city + ': ' + convertToCelsius(parsedData.main.temp) + ' Celsius');
-    }
-}
-
-function convertToCelsius(temperature: number) {
-    return (temperature - 273.15).toFixed(2);
-}
+// function gatherData() {
+//     for (let page of config.pages) {
+//         http.get(page.url, function(res) {
+//             let data: string = '';
+//             res.on('data', function(chunk) {
+//                 data += chunk;
+//             });
+//             res.on('end', function() {
+//                 prepareData(data, page);
+//             });
+//         }).on('error', function(error) {
+//             console.log('error with: ' + page + '\n' + error.message);
+//         });
+//     }
+// } setInterval(gatherData, config.intervalDuration);
+//
+// function prepareData(data: string, page: any) {
+//     let parsedData: any = JSON.parse(data);
+//     if (_.has(parsedData, 'main.temp')) {
+//         console.log(_.now() + ' ' + page.city + ': ' + convertToCelsius(parsedData.main.temp) + ' Celsius');
+//     }
+// }
+//
+// function convertToCelsius(temperature: number) {
+//     return (temperature - 273.15).toFixed(2);
+// }
 
